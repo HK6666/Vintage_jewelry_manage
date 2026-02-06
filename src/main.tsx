5555533804d80728a342812ec541918207ac6400
Hk <1142135247@qq.com>
2026-02-06 14:45:39 +0800

    feat: update dist assets and main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Vintage_jewelry_manage/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
